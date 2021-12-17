48e2377ec7c55f413ae4e9f3cd3e152563e04b7e
A Subhash <94886211+AVNSubhash@users.noreply.github.com>
2021-12-17 18:01:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var a=prompt("Enter UserName");
  var b=prompt("Enter password");
   	if(a== "SP" && b=="SP")
   	{
-  	    window.location.href="start.html";
+  	    window.location.href="https://www.google.com";
   	}
   	else
   	{
   		alert("invalid details");
-  		window.location.href="https://www.google.com";
+  		window.location.href="index.html";
   	}
   	
